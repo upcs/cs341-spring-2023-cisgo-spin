ebcf76e195bf521fa016fcdb1b32909cde7eacbf
n8hopp <hopper26@up.edu>
2023-02-10 12:13:01 -0800

    Added opportunity/index.js

--- routes/opportunity/index.js
+++ routes/opportunity/index.js
@@ -6,4 +6,8 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/:id([0-9]{3,}', function(req, res, next) {
+  let sqlRequest = `SELECT * FROM opportunity WHERE opportunity_id ${hi}`;
+})
+
 module.exports = router;
