bcc18c2d59c7572fcd6f955c554ab76f22a7a111
n8hopp <hopper26@up.edu>
2023-03-14 12:10:29 -0700

    Updating some comments

--- models/getAllLocsByCountryCode.model.js
+++ models/getAllLocsByCountryCode.model.js
@@ -1,7 +1,7 @@
 const connection = require('../util/connection');
 
 // This model uses 'connection' which is our db connection script -
-// This specific model calls the query "Select * from location"
+// This specific model calls the query "Select * from location where countrycode = '{code}'"
 exports.getAllLocsByCountryCode = async function getAllLocsByCountryCode(req) {
 	// query sent to db: returns all objects from location
     const sql = `SELECT * FROM location WHERE countrycode='${req.countrycode}'`;
--- models/getOppWithID.model.js
+++ models/getOppWithID.model.js
@@ -1,7 +1,7 @@
 const connection = require('../util/connection');
 
 // This model uses 'connection' which is our db connection script -
-// This specific model calls the query "Select * from opportunity"
+// This specific model calls the query "Select * from opportunity where opportunity id = {id}"
 exports.getOppWithID = async function getOppWithID(req) {
 	// query sent to db: selects specific 1 object from db from matching id
     const sql = `SELECT * FROM opportunity WHERE opportunity_id=${req.opportunity_id}`;
