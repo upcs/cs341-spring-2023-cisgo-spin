8984964afd6e9f9b579e3881646a6995cb291bed
7angsan <97865929+7angsan@users.noreply.github.com>
2023-03-16 11:54:13 -0700

    attempting to add endpoints

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var demoRouter = require('./routes/demo');
 var opportunitiesRouter = require('./routes/opportunities');
+var adminRouter = require('./routes/admin');
 
 var app = express();
 
@@ -29,6 +30,7 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/demo', demoRouter);
 app.use('/opportunities', opportunitiesRouter);
+app.use('/admin', adminRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
